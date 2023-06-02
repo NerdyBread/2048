e9f364e32de67c3c1b69ad35ccbbd845e746a1a3
Sam Fondacaro <samfondacaro2006@gmail.com>
2023-06-02 13:44:59 -0400

    Added x y coordinate system

--- board.py
+++ board.py
@@ -6,14 +6,14 @@ class Board:
 	def __init__(self, size):
 		self.size = size
 		self.grid = []
+		self.filled = []
 		self._buildGrid()
-		self.empty = (size**2) - 2 # Because two spaces are filled by self.start()
 		self.start()
 		
 	def _buildGrid(self):
 		"""Creates grid of x size when the board is initialized"""
 		for x in range(self.size):
-			self.grid.append([0 for y in range(self.size)])
+			self.grid.append([Tile(None, x, y) for y in range(self.size)])
 		
 	def createTile(self) -> Tile:
 		"""Returns a 4 tile 10% of the time and a 2 tile the other 90%"""
@@ -21,23 +21,22 @@ class Board:
 			return Tile(4)
 		else:
 			return Tile(2)
-		# TODO Fix this
 	
 	def addRandomTile(self):
 		"""Adds tile to random empty spot on the board"""
 		newTile = self.createTile()
-		empty = [] # TODO fix this ugly ass code
-		for i, row in enumerate(self.grid):
-			for j, tile in enumerate(row):
-				if tile == 0:
-					empty.append([i, j])
-		pos = random.choice(empty)
-		x = pos[0]
-		y = pos[1]
-		self.grid[x][y] = newTile # Why does it just ignore this line
+		empty = []
+		for row in self.grid:
+			for tile in row:
+				if tile not in self.filled:
+					empty.append(tile)
+		newPos = random.choice(empty).position
+		self.grid[newPos["x"]][newPos["y"]] = newTile
+		self.filled.append(newTile)
+		newTile.position = newPos
 		# DONE Create a tile
 		# DONE Find a random !empty! position for the it
-		# TODO Put it at that position
+		# DONE Put it at that position
 	
 	def start(self):
 		"""Add two tiles to random spots at the start of the game"""
@@ -48,18 +47,20 @@ class Board:
 		out = ""
 		for row in self.grid:
 			for tile in row:
-				if not tile:
+				if not tile.val:
 					out += "[ ]"
 				else:
 					out += f"[{str(tile)}]"
 			out += "\n"
 		return out
-	
 		
-	def move(self, direction):
-		"""Board updates according to input and adds new tile"""
-		# Empty 
-		pass
+	def directionMap(self, direction):
+		directions = {"r": {"x": 1, "y": 0},
+                "l": {"x": -1, "y": 0},
+                "u": {"x": 0, "y": -1},
+                "d": {"x": 0, "y": 1}}
+		return directions[direction]
+	
 		
 # Ok so what does this board need to do (backend)
 """
@@ -68,8 +69,8 @@ class Board:
 2a. Move tiles to the correct new position
 2b. Combine like tiles
 2b. Collide unlike tiles
-3. Add a tile to a random spot
-3a. Add two tiles to random spots at the beginning of the game
+3. Add a tile to a random spot DONE
+3a. Add two tiles to random spots at the beginning of the game DONE
 4. Make sure the move is valid? (non-valid moves just wouldn't do anything so maybe not)
 """
 
--- tile.py
+++ tile.py
@@ -1,6 +1,8 @@
 class Tile:
-	def __init__(self, val):
+	def __init__(self, val, x=None, y=None):
 		self.val = val
+		self.moved = bool() # Check if tile has moved this turn
+		self.position = {"x": x, "y": y}
 		
 	def __str__(self):
 		return str(self.val)
